de72a923b4467bb7c57246f196f6b38be60802c0
TrishBatra18 <trish.idesign.market@gmail.com>
2024-07-12 17:26:33 +0530

    path corrected

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -5,7 +5,7 @@ const path =  require("path")
 
 const storage = multer.diskStorage({
   destination: (req,file,cb)=>{
-    cb(null, '../frontend/Public/Images')
+    cb(null, '../frontend/public/Images')
   },
   filename: (req,file,cb)=>{
     console.log("file", file)
